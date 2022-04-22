2220bb781e824e7549cc611e490191dc0e79dfd9
Juan Pablo <jpaguilarc@eafit.edu.co>
2022-04-22 11:06:48 -0500

    Comentarios que ayudan a comprender la funcion

--- dataframes.py
+++ dataframes.py
@@ -4,9 +4,9 @@ import math
 palabra = input("Escriba una cadena de caracteres: ")
 
 def palabra_palindromo(palabra: str)-> str:
-    inv = ""
+    inv = "" #String vacío que almacena la palabra invertida
     for letra in palabra:
         inv = letra + palabra
     return inv 
 
-palindromo = palabra_palindromo(palabra)
+palindromo = palabra_palindromo(palabra) #Variable que guarda el retorno de la function
